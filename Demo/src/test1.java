fdcf86e60beb0ce3376861098ffc44a29782c430
Nhut Tran <nhut@lexor.com>
2017-02-17 16:13:13 -0800

    Demo lay bien moi truong (Get Environment Variables)

--- Demo/src/test1.java
+++ Demo/src/test1.java
@@ -9,12 +9,22 @@ import testlink.api.java.client.TestLinkAPIClient;
 import testlink.api.java.client.TestLinkAPIException;
 import testlink.api.java.client.TestLinkAPIResults;
 
+import java.util.Map;
+
 public class test1 {
 	public String URL = "http://shoplexor.mobi/System/Login.aspx";
 	public WebDriver driver;
+
 	@Test
 	public void demo(){
+		String s;
+    	s = System.getenv("UserProfile");
+    	s = s + "\\chromedriver.exe";
+    	
+    	System.out.println(s);
+    	
 		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\driver\\chromedriver.exe");
+		
 		driver=new ChromeDriver();
 		driver.get(URL);
 		driver.findElement(By.id("txt_Username")).sendKeys("TucLy");
@@ -26,6 +36,22 @@ public class test1 {
 		driver.findElement(By.xpath("//*[@id=\"ctl00_A2\"]")).click();
 		driver.findElement(By.xpath("//*[@id=\"ctl00_A10\"]")).click();
 		driver.quit();
+		}
+
+	    public String GetProfileName () {
+	    	
+//	    	Map<String, String> env = System.getenv();
+//	        for (String envName : env.keySet()) {
+//	            System.out.format("%s=%s%n",
+//	                              envName,
+//	                              env.get(envName));
+//	        }
+
+	    	
+	    	String s;
+	    	s = System.getenv("UserProfile");
+	    	
+	        return(s);
 		}
 
 	}
